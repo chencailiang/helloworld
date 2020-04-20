1ba5bf9a290d772588c601a12cd397e8ed364718
chencailiang <32255411+chencailiang@users.noreply.github.com>
2020-04-20 22:21:42 +0800

    Update Helloworld.java

--- src/main/java/com/ccl/Helloworld.java
+++ src/main/java/com/ccl/Helloworld.java
@@ -3,6 +3,6 @@ package com.ccl;
 public class Helloworld {
 
     public static void main(String[] args) {
-        System.out.println("hello world! idea1");
+        System.out.println("hello world! github2");
     }
 }
