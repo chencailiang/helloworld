1cc6781c5bbd085f1d6fe48560b9cb884c6344e4
ccl <979377400@qq.com>
2020-04-20 22:15:14 +0800

    修改test11

--- src/main/java/com/ccl/Helloworld.java
+++ src/main/java/com/ccl/Helloworld.java
@@ -3,6 +3,6 @@ package com.ccl;
 public class Helloworld {
 
     public static void main(String[] args) {
-        System.out.println("hello world! github");
+        System.out.println("hello world! idea");
     }
 }
