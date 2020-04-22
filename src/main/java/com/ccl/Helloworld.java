9e3baedb8a687281212c06465e46e31a71396f67
chencailiang <32255411+chencailiang@users.noreply.github.com>
2020-04-22 08:35:11 +0800

    Update Helloworld.java

--- src/main/java/com/ccl/Helloworld.java
+++ src/main/java/com/ccl/Helloworld.java
@@ -4,6 +4,5 @@ public class Helloworld {
 
     public static void main(String[] args) {
         System.out.println("hello world! idea4");
-        System.out.println("hello world! github4");
     }
 }
