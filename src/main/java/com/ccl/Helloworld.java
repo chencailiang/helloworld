94d4848a8700c7103ae95c56c795dd6ec303dc50
ccl <979377400@qq.com>
2020-04-23 09:11:25 +0800

    添加demo文件

--- src/main/java/com/ccl/Helloworld.java
+++ src/main/java/com/ccl/Helloworld.java
@@ -1,11 +1,15 @@
 package com.ccl;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class Helloworld {
 
     public static void main(String[] args) {
-        System.out.println("hello world! idea4");
-        System.out.println("hello world! github666");
-
-
+        Date date = new Date() ;
+        Calendar dd = Calendar.getInstance( );
+        dd.setTime(date);
+        int month = dd.get(Calendar.MONTH)+1;
+        System.out.println(month);
     }
 }
